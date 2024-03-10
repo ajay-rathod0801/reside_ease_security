6a1a31c5bbd6a581b4fa73b4cbd8bdacc10fe7d0
Ajay Rathod <ajay.rathod0801@gmail.com>
2024-03-10 11:18:52 +0530

    update profile & app icons

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -5,6 +5,64 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 130,
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person,
+                    size: 130,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 12),
+                OutlinedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.edit_square,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  label: const Text('Edit Profile'),
+                ),
+                const SizedBox(height: 12),
+                Text(
+                  'John Doe',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  '+91 9876543210',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  width: double.infinity,
+                  child: FilledButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.error,
+                    ),
+                    onPressed: () {},
+                    icon: Icon(Icons.logout_outlined),
+                    label: const Text('Logout'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
